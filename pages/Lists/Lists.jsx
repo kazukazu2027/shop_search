c7a6a2ae29e1798afcc0790d403f3aaf06d1b158
kazukazu2027 <kazu2000027@gmail.com>
2021-05-06 23:22:57 +0900

    apikey

--- pages/Lists/Lists.jsx
+++ pages/Lists/Lists.jsx
@@ -2,13 +2,12 @@ import List from '../../components/DataLists'
 import axios from 'axios';
 import axiosJsonAdapter from 'axios-jsonp';
 import useSWR from 'swr';
-import API_KEY from '../../api';
 
-{console.log(process.env.API_KEY)}
+// {console.log(process.env.API_KEY)}
 const fetcher = () => {
     return new Promise ((resolve) => {
         const success = (position) => {
-            axios.get(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&lat=${position.coords.latitude}&lng=${position.coords.longitude}&genre=G001&count=20&format=jsonp`,
+            axios.get(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&lat=${position.coords.latitude}&lng=${position.coords.longitude}&genre=G001&count=20&format=jsonp`,
             {
                 adapter: axiosJsonAdapter,
             }
--- pages/Lists/[id].js
+++ pages/Lists/[id].js
@@ -1,9 +1,8 @@
-import API_KEY from "../../api";
 import { List } from "../../components/List";
 
 export async function getServerSideProps({params}){
     const id = params.id;
-    const res = await fetch(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&id=${id}&format=json`);
+    const res = await fetch(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&id=${id}&format=json`);
     const json = await res.json();
     const lists = json.results.shop;
     const list = lists[0];
